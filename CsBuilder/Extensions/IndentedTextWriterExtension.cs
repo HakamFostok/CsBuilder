5d6e32a515dacf916c75df7f925840d7484e72c5
Hakan Fıstık <hakam.fstk@gmail.com>
2022-05-19 15:20:56 +0300

    Add project files.

---
+++ CsBuilder/Extensions/IndentedTextWriterExtension.cs
@@ -0,0 +1,18 @@
+﻿using System.CodeDom.Compiler;
+
+namespace CsBuilder;
+
+internal static class IndentedTextWriterExtension
+{
+    internal static void GoOneLevelInside(this IndentedTextWriter indentedTextWriter)
+    {
+        indentedTextWriter.WriteLine("{");
+        indentedTextWriter.Indent++;
+    }
+
+    internal static void GoOneLevelOutside(this IndentedTextWriter indentedTextWriter)
+    {
+        indentedTextWriter.Indent--;
+        indentedTextWriter.WriteLine("}");
+    }
+}
